4876145e2b5dbdbfe386e34ee09d2de97b802b39
ROG\piotr <piotr.dunaj95@gmail.com>
2019-05-16 22:09:44 +0200

    fixes, new endpoints

--- routes/cars.js
+++ routes/cars.js
@@ -21,7 +21,65 @@ router.get('/', LoginValidator, function (req, res, next) {
                 ]
         })
         .then(cars => {
-            res.render('cars_table', {title: "Nasze pojazdy", users: cars})
+            res.render('cars_table', {title: "Nasze pojazdy", cars: cars})
+        })
+        .catch(reason => {
+            res.render('error_page', {message: reason.message})
+        })
+});
+
+router.get('/:id([0-9]+)', LoginValidator, function (req, res, next) {
+    let carId = req.params.id;
+
+    CarsModel
+        .findAll({
+            where: {id: carId},
+            include:
+                [
+                    {
+                        model: ModelModel,
+                        include: [
+                            BrandModel
+                        ]
+                    }
+                ]
+        })
+        .then(car => {
+            if (car.length !== 1) {
+                res.render('error_page', {message: "Nie znaleziono pojazdu o id: " + carId});
+                return;
+            }
+
+            res.render('car_details', {title: "Szczegóły pojazdu", car: car[0]})
+        })
+        .catch(reason => {
+            res.render('error_page', {message: reason.message})
+        })
+});
+
+router.get('/:id([0-9]+)/reservation', LoginValidator, function (req, res, next) {
+    let carId = req.params.id;
+
+    CarsModel
+        .findAll({
+            where: {id: carId},
+            include:
+                [
+                    {
+                        model: ModelModel,
+                        include: [
+                            BrandModel
+                        ]
+                    }
+                ]
+        })
+        .then(car => {
+            if (car.length !== 1) {
+                res.render('error_page', {message: "Nie znaleziono pojazdu o id: " + carId});
+                return;
+            }
+
+            res.render('car_reservation', {title: "Rezerwacja pojazdu", car: car[0]})
         })
         .catch(reason => {
             res.render('error_page', {message: reason.message})
